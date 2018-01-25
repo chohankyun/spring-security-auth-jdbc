bcbc652d9b3df8ec61397c59cb4f60ecf77de90f
chohankyun <chohankyun@kolon.com>
2018-01-25 10:24:17 +0900

    1.sql 수정

--- spring-security-auth-server/oauth.sql
+++ spring-security-auth-server/oauth.sql
@@ -44,3 +44,10 @@ create table oauth_approvals (
 	expiresAt TIMESTAMP,
 	lastModifiedAt TIMESTAMP
 );
+
+
+
+insert into oauth_client_details 
+(client_id, client_secret, resource_ids, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove, logout_uri, base_uri) 
+values 
+('ClientId', 'secret', null, 'read', 'authorization_code', null, 'ROLE_ADMIN', 36000, 2592000, null, 'true', null, null);
